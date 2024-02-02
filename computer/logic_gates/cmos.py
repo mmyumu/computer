1bd281f1151c53e48b6d34ce8e64fccdbd73ac2c
mmyumu <mmyumu@gmail.com>
2024-02-02 01:28:24 +0100

    Add FullAdder

--- computer/logic_gates/cmos.py
+++ computer/logic_gates/cmos.py
@@ -1,6 +1,7 @@
 """
 CMOS Gates implementations
 """
+from typing import Tuple
 from computer.electronic.transistor import NMOSTransistor
 from computer.electronic.transistor import PMOSTransistor
 from computer.logic_gates.voltage_levels import GND, VDD
@@ -208,18 +209,49 @@ class XNORGate:
 
 class HalfAdder:
     """
-    Add 2 bits and returns the sum and the carry
+    Add 2 bits: A and B
+    Returns the sum and the carry out
+    https://www.elprocus.com/wp-content/uploads/HA-Logical-Diagram.jpg
     """
     def __init__(self):
         self._xor = XORGate()
         self._and = ANDGate()
 
 
-    def __call__(self, input_signal_a: bool, input_signal_b: bool) -> bool:
+    def __call__(self, input_signal_a: bool, input_signal_b: bool) -> Tuple[bool, bool]:
         """
         Logic gate operates inputs and returns outputs
         """
         sum_result = self._xor(input_signal_a, input_signal_b)
         carry = self._and(input_signal_a, input_signal_b)
+
+        return sum_result, carry
+
+
+class FullAdder:
+    """
+    Add 3 bits: A, B and carry in
+    Returns the sum and the carry out
+    https://www.elprocus.com/wp-content/uploads/Full-Adder-Logical-Diagram.png
+    """
+    def __init__(self):
+        self._xor1 = XORGate()
+        self._xor2 = XORGate()
+        self._and1 = ANDGate()
+        self._and2 = ANDGate()
+        self._or = ORGate()
+
+
+    def __call__(self, input_signal_a: bool, input_signal_b: bool, input_signal_carry_in: bool) -> Tuple[bool, bool]:
+        """
+        Logic gate operates inputs and returns outputs
+        """
+        xor1 = self._xor1(input_signal_a, input_signal_b)
+        sum_result = self._xor2(xor1, input_signal_carry_in)
+
+        and1 = self._and1(input_signal_carry_in, xor1)
+        and2 = self._and2(input_signal_a, input_signal_b)
+
+        carry = self._or(and1, and2)
 
         return sum_result, carry
--- tests/computer/logic_gates/test_cmos.py
+++ tests/computer/logic_gates/test_cmos.py
@@ -2,7 +2,7 @@
 Test for CMOS logic gates
 """
 import pytest
-from computer.logic_gates.cmos import ANDGate, HalfAdder, NANDGate, NORGate, NOTGate, ORGate, XNORGate, XORGate
+from computer.logic_gates.cmos import ANDGate, FullAdder, HalfAdder, NANDGate, NORGate, NOTGate, ORGate, XNORGate, XORGate
 
 # pylint: disable=C0116
 
@@ -37,6 +37,10 @@ def fixture_cmos_xnor_gate():
 @pytest.fixture(name="half_adder")
 def fixture_half_adder():
     return HalfAdder()
+
+@pytest.fixture(name="full_adder")
+def fixture_full_adder():
+    return FullAdder()
 
 
 def test_not_gate_true(cmos_not_gate: NOTGate):
@@ -135,4 +139,44 @@ def test_halfadder_a_false_b_true(half_adder: HalfAdder):
 def test_halfadder_a_true_b_true(half_adder: HalfAdder):
     sum_result, carry = half_adder(True, True)
     assert sum_result is False
+    assert carry is True
+
+def test_fulladder_a_false_b_false_c_false(full_adder: FullAdder):
+    sum_result, carry = full_adder(False, False, False)
+    assert sum_result is False
+    assert carry is False
+
+def test_fulladder_a_false_b_false_c_true(full_adder: FullAdder):
+    sum_result, carry = full_adder(False, False, True)
+    assert sum_result is True
+    assert carry is False
+
+def test_fulladder_a_false_b_true_c_false(full_adder: FullAdder):
+    sum_result, carry = full_adder(False, True, False)
+    assert sum_result is True
+    assert carry is False
+
+def test_fulladder_a_false_b_true_c_true(full_adder: FullAdder):
+    sum_result, carry = full_adder(False, True, True)
+    assert sum_result is False
+    assert carry is True
+
+def test_fulladder_a_true_b_false_c_false(full_adder: FullAdder):
+    sum_result, carry = full_adder(True, False, False)
+    assert sum_result is True
+    assert carry is False
+
+def test_fulladder_a_true_b_false_c_true(full_adder: FullAdder):
+    sum_result, carry = full_adder(True, False, True)
+    assert sum_result is False
+    assert carry is True
+
+def test_fulladder_a_true_b_true_c_false(full_adder: FullAdder):
+    sum_result, carry = full_adder(True, True, False)
+    assert sum_result is False
+    assert carry is True
+
+def test_fulladder_a_true_b_true_c_true(full_adder: FullAdder):
+    sum_result, carry = full_adder(True, True, True)
+    assert sum_result is True
     assert carry is True
