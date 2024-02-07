b9782ab2f514583dc75930507d596c35bdcf57f9
mmyumu <mmyumu@gmail.com>
2024-02-07 01:52:35 +0100

    Add decoder 3 to 8

--- computer/electronic/circuits/decoder.py
+++ computer/electronic/circuits/decoder.py
@@ -2,7 +2,7 @@
 Binary decoders
 """
 from typing import Tuple
-from computer.electronic.circuits.cmos import ANDGate, NOTGate
+from computer.electronic.circuits.cmos import ANDGate, ANDGate3, NOTGate
 
 
 class Decoder2To4:
@@ -34,3 +34,43 @@ class Decoder2To4:
 
 
         return d3, d2, d1, d0
+
+
+class Decoder3To8:
+    """
+    3-to-8 binary decoder
+    https://www.101computing.net/binary-decoders-using-logic-gates/
+    Not really the link above since they reverted A2, A1, A0 between truth table and diagram
+    """
+    def __init__(self):
+        self._not0 = NOTGate()
+        self._not1 = NOTGate()
+        self._not2 = NOTGate()
+
+        self._and0 = ANDGate3()
+        self._and1 = ANDGate3()
+        self._and2 = ANDGate3()
+        self._and3 = ANDGate3()
+        self._and4 = ANDGate3()
+        self._and5 = ANDGate3()
+        self._and6 = ANDGate3()
+        self._and7 = ANDGate3()
+
+    def __call__(self, input_signal_a2: bool, input_signal_a1: bool, input_signal_a0: bool) -> Tuple[bool, bool, bool, bool]:
+        """
+        Logic gate operates inputs and returns outputs
+        """
+        not0 = self._not0(input_signal_a0)
+        not1 = self._not1(input_signal_a1)
+        not2 = self._not1(input_signal_a2)
+
+        d0 = self._and0(not0, not1, not2)
+        d1 = self._and1(input_signal_a0, not1, not2)
+        d2 = self._and2(not0, input_signal_a1, not2)
+        d3 = self._and3(input_signal_a0, input_signal_a1, not2)
+        d4 = self._and3(not0, not1, input_signal_a2)
+        d5 = self._and3(input_signal_a0, not1, input_signal_a2)
+        d6 = self._and3(not0, input_signal_a1, input_signal_a2)
+        d7 = self._and3(input_signal_a0, input_signal_a1, input_signal_a2)
+
+        return d7, d6, d5, d4, d3, d2, d1, d0
--- tests/computer/electronic/circuits/test_decoder.py
+++ tests/computer/electronic/circuits/test_decoder.py
@@ -2,7 +2,7 @@
 Test for Decoders
 """
 import pytest
-from computer.electronic.circuits.decoder import Decoder2To4
+from computer.electronic.circuits.decoder import Decoder2To4, Decoder3To8
 
 # pylint: disable=C0116
 
@@ -10,6 +10,9 @@ from computer.electronic.circuits.decoder import Decoder2To4
 def fixture_decoder2to4():
     return Decoder2To4()
 
+@pytest.fixture(name="decoder3to8")
+def fixture_decoder3to8():
+    return Decoder3To8()
 
 def test_decoder2to4_a1_false_a0_false(decoder2to4: Decoder2To4):
     d3, d2, d1, d0 = decoder2to4(False, False)
@@ -35,6 +38,94 @@ def test_decoder2to4_a1_true_a0_false(decoder2to4: Decoder2To4):
 def test_decoder2to4_a1_true_a0_true(decoder2to4: Decoder2To4):
     d3, d2, d1, d0 = decoder2to4(True, True)
     assert d3 is True
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_false_a1_false_a0_false(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, False, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is True
+
+def test_decoder3to8_a2_false_a1_false_a0_true(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, False, True)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is True
+    assert d0 is False
+
+def test_decoder3to8_a2_false_a1_true_a0_false(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, True, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is True
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_false_a1_true_a0_true(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, True, True)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is True
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_true_a1_false_a0_false(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, False, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is True
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_true_a1_false_a0_true(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, False, True)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is True
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_true_a1_true_a0_false(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, True, False)
+    assert d7 is False
+    assert d6 is True
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_true_a1_true_a0_true(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, True, True)
+    assert d7 is True
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
     assert d2 is False
     assert d1 is False
     assert d0 is False
