e8c57a9f9db34f8db50ce65333eed8a36f428e3e
mmyumu <mmyumu@gmail.com>
2024-02-07 02:18:58 +0100

    Add decoder 4 to 16

--- computer/electronic/circuits/decoder.py
+++ computer/electronic/circuits/decoder.py
@@ -14,23 +14,23 @@ class Decoder2To4:
         self._not0 = NOTGate()
         self._not1 = NOTGate()
 
-        self._and0 = ANDGate()
-        self._and1 = ANDGate()
-        self._and2 = ANDGate()
-        self._and3 = ANDGate()
+        self._and0 = ANDGate3()
+        self._and1 = ANDGate3()
+        self._and2 = ANDGate3()
+        self._and3 = ANDGate3()
 
 
-    def __call__(self, input_signal_a1: bool, input_signal_a0: bool) -> Tuple[bool, bool, bool, bool]:
+    def __call__(self, input_signal_a1: bool, input_signal_a0: bool, enable: bool) -> Tuple[bool, bool, bool, bool]:
         """
         Logic gate operates inputs and returns outputs
         """
         not0 = self._not0(input_signal_a0)
         not1 = self._not1(input_signal_a1)
 
-        d0 = self._and0(not0, not1)
-        d1 = self._and1(not1, input_signal_a0)
-        d2 = self._and2(not0, input_signal_a1)
-        d3 = self._and3(input_signal_a0, input_signal_a1)
+        d0 = self._and0(not0, not1, enable)
+        d1 = self._and1(not1, input_signal_a0, enable)
+        d2 = self._and2(not0, input_signal_a1, enable)
+        d3 = self._and3(input_signal_a0, input_signal_a1, enable)
 
 
         return d3, d2, d1, d0
@@ -43,34 +43,91 @@ class Decoder3To8:
     Not really the link above since they reverted A2, A1, A0 between truth table and diagram
     """
     def __init__(self):
-        self._not0 = NOTGate()
-        self._not1 = NOTGate()
-        self._not2 = NOTGate()
+        self._not = NOTGate()
+        self._decoder2to4_0 = Decoder2To4()
+        self._decoder2to4_1 = Decoder2To4()
 
-        self._and0 = ANDGate3()
-        self._and1 = ANDGate3()
-        self._and2 = ANDGate3()
-        self._and3 = ANDGate3()
-        self._and4 = ANDGate3()
-        self._and5 = ANDGate3()
-        self._and6 = ANDGate3()
-        self._and7 = ANDGate3()
+        self._and0 = ANDGate()
+        self._and1 = ANDGate()
+        self._and2 = ANDGate()
+        self._and3 = ANDGate()
+        self._and4 = ANDGate()
+        self._and5 = ANDGate()
+        self._and6 = ANDGate()
+        self._and7 = ANDGate()
 
-    def __call__(self, input_signal_a2: bool, input_signal_a1: bool, input_signal_a0: bool) -> Tuple[bool, bool, bool, bool]:
+    def __call__(self, input_signal_a2: bool, input_signal_a1: bool, input_signal_a0: bool, enable: bool) -> Tuple[bool, bool, bool, bool]:
         """
         Logic gate operates inputs and returns outputs
         """
-        not0 = self._not0(input_signal_a0)
-        not1 = self._not1(input_signal_a1)
-        not2 = self._not1(input_signal_a2)
+        not2 = self._not(input_signal_a2)
+        d3, d2, d1, d0 = self._decoder2to4_0(input_signal_a1, input_signal_a0, not2)
+        d7, d6, d5, d4 = self._decoder2to4_1(input_signal_a1, input_signal_a0, input_signal_a2)
 
-        d0 = self._and0(not0, not1, not2)
-        d1 = self._and1(input_signal_a0, not1, not2)
-        d2 = self._and2(not0, input_signal_a1, not2)
-        d3 = self._and3(input_signal_a0, input_signal_a1, not2)
-        d4 = self._and3(not0, not1, input_signal_a2)
-        d5 = self._and3(input_signal_a0, not1, input_signal_a2)
-        d6 = self._and3(not0, input_signal_a1, input_signal_a2)
-        d7 = self._and3(input_signal_a0, input_signal_a1, input_signal_a2)
+        d0 = self._and0(d0, enable)
+        d1 = self._and0(d1, enable)
+        d2 = self._and0(d2, enable)
+        d3 = self._and0(d3, enable)
+        d4 = self._and0(d4, enable)
+        d5 = self._and0(d5, enable)
+        d6 = self._and0(d6, enable)
+        d7 = self._and0(d7, enable)
 
         return d7, d6, d5, d4, d3, d2, d1, d0
+
+
+class Decoder4To16:
+    """
+    4-to-16 binary decoder
+    https://www.tutorialspoint.com/digital_circuits/digital_circuits_decoders.htm
+    """
+    def __init__(self):
+        self._not = NOTGate()
+        self._decoder3to8_0 = Decoder3To8()
+        self._decoder3to8_1 = Decoder3To8()
+
+        self._and0 = ANDGate()
+        self._and1 = ANDGate()
+        self._and2 = ANDGate()
+        self._and3 = ANDGate()
+        self._and4 = ANDGate()
+        self._and5 = ANDGate()
+        self._and6 = ANDGate()
+        self._and7 = ANDGate()
+        self._and8 = ANDGate()
+        self._and9 = ANDGate()
+        self._and10 = ANDGate()
+        self._and11 = ANDGate()
+        self._and12 = ANDGate()
+        self._and13 = ANDGate()
+        self._and14 = ANDGate()
+        self._and15 = ANDGate()
+
+    def __call__(self, input_signal_a3: bool, input_signal_a2: bool, input_signal_a1: bool, input_signal_a0:
+                 bool, enable: bool) -> Tuple[bool, bool, bool, bool]:
+        """
+        Logic gate operates inputs and returns outputs
+        """
+        not3 = self._not(input_signal_a3)
+
+        d7, d6, d5, d4, d3, d2, d1, d0 = self._decoder3to8_0(input_signal_a2, input_signal_a1, input_signal_a0, not3)
+        d15, d14, d13, d12, d11, d10, d9, d8 = self._decoder3to8_0(input_signal_a2, input_signal_a1, input_signal_a0, input_signal_a3)
+
+        d0 = self._and0(d0, enable)
+        d1 = self._and0(d1, enable)
+        d2 = self._and0(d2, enable)
+        d3 = self._and0(d3, enable)
+        d4 = self._and0(d4, enable)
+        d5 = self._and0(d5, enable)
+        d6 = self._and0(d6, enable)
+        d7 = self._and0(d7, enable)
+        d8 = self._and0(d8, enable)
+        d9 = self._and0(d9, enable)
+        d10 = self._and0(d10, enable)
+        d11 = self._and0(d11, enable)
+        d12 = self._and0(d12, enable)
+        d13 = self._and0(d13, enable)
+        d14 = self._and0(d14, enable)
+        d15 = self._and0(d15, enable)
+
+        return d15, d14, d13, d12, d11, d10, d9, d8, d7, d6, d5, d4, d3, d2, d1, d0
--- tests/computer/electronic/circuits/test_decoder.py
+++ tests/computer/electronic/circuits/test_decoder.py
@@ -2,7 +2,7 @@
 Test for Decoders
 """
 import pytest
-from computer.electronic.circuits.decoder import Decoder2To4, Decoder3To8
+from computer.electronic.circuits.decoder import Decoder2To4, Decoder3To8, Decoder4To16
 
 # pylint: disable=C0116
 
@@ -14,36 +14,69 @@ def fixture_decoder2to4():
 def fixture_decoder3to8():
     return Decoder3To8()
 
-def test_decoder2to4_a1_false_a0_false(decoder2to4: Decoder2To4):
-    d3, d2, d1, d0 = decoder2to4(False, False)
+@pytest.fixture(name="decoder4to16")
+def fixture_decoder4to16():
+    return Decoder4To16()
+
+
+def test_decoder2to4_a1_false_a0_false_enabled(decoder2to4: Decoder2To4):
+    d3, d2, d1, d0 = decoder2to4(False, False, True)
     assert d3 is False
     assert d2 is False
     assert d1 is False
     assert d0 is True
 
-def test_decoder2to4_a1_false_a0_true(decoder2to4: Decoder2To4):
-    d3, d2, d1, d0 = decoder2to4(False, True)
+def test_decoder2to4_a1_false_a0_true_enabled(decoder2to4: Decoder2To4):
+    d3, d2, d1, d0 = decoder2to4(False, True, True)
     assert d3 is False
     assert d2 is False
     assert d1 is True
     assert d0 is False
 
-def test_decoder2to4_a1_true_a0_false(decoder2to4: Decoder2To4):
-    d3, d2, d1, d0 = decoder2to4(True, False)
+def test_decoder2to4_a1_true_a0_false_enabled(decoder2to4: Decoder2To4):
+    d3, d2, d1, d0 = decoder2to4(True, False, True)
     assert d3 is False
     assert d2 is True
     assert d1 is False
     assert d0 is False
 
-def test_decoder2to4_a1_true_a0_true(decoder2to4: Decoder2To4):
-    d3, d2, d1, d0 = decoder2to4(True, True)
+def test_decoder2to4_a1_true_a0_true_enabled(decoder2to4: Decoder2To4):
+    d3, d2, d1, d0 = decoder2to4(True, True, True)
     assert d3 is True
     assert d2 is False
     assert d1 is False
     assert d0 is False
 
-def test_decoder3to8_a2_false_a1_false_a0_false(decoder3to8: Decoder3To8):
-    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, False, False)
+def test_decoder2to4_a1_false_a0_false_disabled(decoder2to4: Decoder2To4):
+    d3, d2, d1, d0 = decoder2to4(False, False, False)
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder2to4_a1_false_a0_true_disabled(decoder2to4: Decoder2To4):
+    d3, d2, d1, d0 = decoder2to4(False, True, False)
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder2to4_a1_true_a0_false_disabled(decoder2to4: Decoder2To4):
+    d3, d2, d1, d0 = decoder2to4(True, False, False)
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder2to4_a1_true_a0_true_disabled(decoder2to4: Decoder2To4):
+    d3, d2, d1, d0 = decoder2to4(True, True, False)
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_false_a1_false_a0_false_enabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, False, False, True)
     assert d7 is False
     assert d6 is False
     assert d5 is False
@@ -53,8 +86,8 @@ def test_decoder3to8_a2_false_a1_false_a0_false(decoder3to8: Decoder3To8):
     assert d1 is False
     assert d0 is True
 
-def test_decoder3to8_a2_false_a1_false_a0_true(decoder3to8: Decoder3To8):
-    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, False, True)
+def test_decoder3to8_a2_false_a1_false_a0_true_enabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, False, True, True)
     assert d7 is False
     assert d6 is False
     assert d5 is False
@@ -64,8 +97,8 @@ def test_decoder3to8_a2_false_a1_false_a0_true(decoder3to8: Decoder3To8):
     assert d1 is True
     assert d0 is False
 
-def test_decoder3to8_a2_false_a1_true_a0_false(decoder3to8: Decoder3To8):
-    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, True, False)
+def test_decoder3to8_a2_false_a1_true_a0_false_enabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, True, False, True)
     assert d7 is False
     assert d6 is False
     assert d5 is False
@@ -75,8 +108,8 @@ def test_decoder3to8_a2_false_a1_true_a0_false(decoder3to8: Decoder3To8):
     assert d1 is False
     assert d0 is False
 
-def test_decoder3to8_a2_false_a1_true_a0_true(decoder3to8: Decoder3To8):
-    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, True, True)
+def test_decoder3to8_a2_false_a1_true_a0_true_enabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, True, True, True)
     assert d7 is False
     assert d6 is False
     assert d5 is False
@@ -86,8 +119,8 @@ def test_decoder3to8_a2_false_a1_true_a0_true(decoder3to8: Decoder3To8):
     assert d1 is False
     assert d0 is False
 
-def test_decoder3to8_a2_true_a1_false_a0_false(decoder3to8: Decoder3To8):
-    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, False, False)
+def test_decoder3to8_a2_true_a1_false_a0_false_enabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, False, False, True)
     assert d7 is False
     assert d6 is False
     assert d5 is False
@@ -97,8 +130,8 @@ def test_decoder3to8_a2_true_a1_false_a0_false(decoder3to8: Decoder3To8):
     assert d1 is False
     assert d0 is False
 
-def test_decoder3to8_a2_true_a1_false_a0_true(decoder3to8: Decoder3To8):
-    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, False, True)
+def test_decoder3to8_a2_true_a1_false_a0_true_enabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, False, True, True)
     assert d7 is False
     assert d6 is False
     assert d5 is True
@@ -108,8 +141,8 @@ def test_decoder3to8_a2_true_a1_false_a0_true(decoder3to8: Decoder3To8):
     assert d1 is False
     assert d0 is False
 
-def test_decoder3to8_a2_true_a1_true_a0_false(decoder3to8: Decoder3To8):
-    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, True, False)
+def test_decoder3to8_a2_true_a1_true_a0_false_enabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, True, False, True)
     assert d7 is False
     assert d6 is True
     assert d5 is False
@@ -119,8 +152,8 @@ def test_decoder3to8_a2_true_a1_true_a0_false(decoder3to8: Decoder3To8):
     assert d1 is False
     assert d0 is False
 
-def test_decoder3to8_a2_true_a1_true_a0_true(decoder3to8: Decoder3To8):
-    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, True, True)
+def test_decoder3to8_a2_true_a1_true_a0_true_enabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, True, True, True)
     assert d7 is True
     assert d6 is False
     assert d5 is False
@@ -129,3 +162,107 @@ def test_decoder3to8_a2_true_a1_true_a0_true(decoder3to8: Decoder3To8):
     assert d2 is False
     assert d1 is False
     assert d0 is False
+
+def test_decoder3to8_a2_false_a1_false_a0_false_disabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, False, False, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_false_a1_false_a0_true_disabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, False, True, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_false_a1_true_a0_false_disabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, True, False, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_false_a1_true_a0_true_disabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(False, True, True, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_true_a1_false_a0_false_disabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, False, False, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_true_a1_false_a0_true_disabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, False, True, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_true_a1_true_a0_false_disabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, True, False, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder3to8_a2_true_a1_true_a0_true_disabled(decoder3to8: Decoder3To8):
+    d7, d6, d5, d4, d3, d2, d1, d0 = decoder3to8(True, True, True, False)
+    assert d7 is False
+    assert d6 is False
+    assert d5 is False
+    assert d4 is False
+    assert d3 is False
+    assert d2 is False
+    assert d1 is False
+    assert d0 is False
+
+def test_decoder4to16_enabled(decoder4to16: Decoder4To16):
+    bit_true_index = 0
+    for a3 in [False, True]:
+        for a2 in [False, True]:
+            for a1 in [False, True]:
+                for a0 in [False, True]:
+                    bits = decoder4to16(a3, a2, a1, a0, True)
+
+                    for bit_index, bit in enumerate(bits[::-1]):
+                        if bit_index == bit_true_index:
+                            assert bit is True, f"Inputs: a3={a3}, a2={a2}, a1={a1}, a0={a0}"
+                        else:
+                            assert bit is False, f"Inputs: a3={a3}, a2={a2}, a1={a1}, a0={a0}"
+
+                    bit_true_index += 1
