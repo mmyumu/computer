791a9c8e50fb89ca1e9c052f9bafedbd6a4171e3
mmyumu <mmyumu@gmail.com>
2024-02-12 23:13:35 +0100

    Add Store instructions

---
+++ computer/instructions/store.py
@@ -0,0 +1,26 @@
+"""
+Store instructions module
+"""
+from computer.data_types import Bits
+from computer.instructions.instruction import Instruction
+
+# pylint: disable=R0903
+class StoreMem(Instruction):
+    """
+    Store from register to memory
+    STORE REG MEM	; stores the value of REG to the address specified
+    """
+    def compute(self, reg1: Bits, reg2: Bits, value: Bits):
+        data = self._registers.read(reg1)
+        self._memory.write(value, data)
+
+
+class StoreReg(Instruction):
+    """
+    Store from register to memory address in register
+    STORE REG REG 	; stores the value of REGA into the memory unit at the address in REGB
+    """
+    def compute(self, reg1: Bits, reg2: Bits, value: Bits):
+        data = self._registers.read(reg1)
+        memory_address = self._registers.read(reg2)
+        self._memory.write(memory_address, data)
---
+++ tests/computer/instructions/test_store.py
@@ -0,0 +1,50 @@
+"""
+Test for Store instructions
+"""
+from computer.data_types import Bits, Data16
+from computer.instructions.store import StoreMem, StoreReg
+from computer.memory import SRAM
+from computer.registers import Registers
+
+
+
+# pylint: disable=C0116
+def test_store_mem(registers: Registers, sram: SRAM):
+    sram.reset()
+    registers.reset()
+
+    d = Data16([1] * 4 + [0] * 4)
+    register_address = Bits(0, 0, 1)
+    registers.write(register_address, d)
+    registers.clock_tick(True)
+
+    memory_address = Bits(0, 0, 1, 1, 0, 0, 1, 1)
+    operand = Bits(register_address + [0] * 3 + memory_address)
+
+    store_mem = StoreMem(registers, sram)
+    store_mem(operand)
+
+    sram.clock_tick(True)
+
+    assert sram.read(memory_address) == tuple(d)
+
+def test_store_reg(registers: Registers, sram: SRAM):
+    sram.reset()
+    registers.reset()
+
+    d = Data16([1] * 4 + [0] * 4)
+    register_address1 = Bits(0, 0, 1)
+    register_address2 = Bits(1, 0, 0)
+    memory_address = Bits(0, 0, 1, 1, 0, 0, 1, 1)
+    registers.write(register_address1, d)
+    registers.write(register_address2, memory_address)
+    registers.clock_tick(True)
+
+    operand = Bits(register_address1 + register_address2 + [0] * 8)
+
+    store_reg = StoreReg(registers, sram)
+    store_reg(operand)
+
+    sram.clock_tick(True)
+
+    assert sram.read(memory_address) == tuple(d)
