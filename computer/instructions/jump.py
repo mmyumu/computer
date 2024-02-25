53974b42f297907a38cc2c548a97c634e82f8530
mmyumu <mmyumu@gmail.com>
2024-02-25 15:05:06 +0100

    Add JLT and JGE instructions

--- computer/instructions/jump.py
+++ computer/instructions/jump.py
@@ -3,6 +3,7 @@ Jump instructions module
 """
 from computer.data_types import Bits
 from computer.electronic.circuits.bitwise import BitwiseMux
+from computer.electronic.circuits.cmos import ORGate
 from computer.instructions.instruction import ControlInstruction
 from computer.memory import SRAM
 from computer.program_counter import ProgramCounter
@@ -31,4 +32,39 @@ class JEQ(ControlInstruction):
         current_address = self._program_counter.value
 
         new_address = self._mux(current_address, value, self._registers.zf)
+        self._program_counter.set(new_address)
+
+
+class JLT(ControlInstruction):
+    """
+    Set the Program Counter to the immediate value if CF = 0
+    JLT IMD		; if CF = 0, sets PC to the immediate 16-bit value 
+    """
+    def __init__(self, registers: Registers, memory: SRAM, program_counter: ProgramCounter) -> None:
+        super().__init__(registers, memory, program_counter)
+        self._mux = BitwiseMux(2 ** self._registers.size)
+
+    def compute(self, reg1: Bits, reg2: Bits, value: Bits):
+        current_address = self._program_counter.value
+
+        new_address = self._mux(value, current_address, self._registers.cf)
+        self._program_counter.set(new_address)
+
+
+class JGE(ControlInstruction):
+    """
+    Set the Program Counter to the immediate value if CF = 1 or ZF = 1
+    JGE IMD		; if CF = 1 or ZF = 1, sets PC to the immediate 16-bit value 
+    """
+    def __init__(self, registers: Registers, memory: SRAM, program_counter: ProgramCounter) -> None:
+        super().__init__(registers, memory, program_counter)
+        self._or = ORGate()
+        self._mux = BitwiseMux(2 ** self._registers.size)
+
+    def compute(self, reg1: Bits, reg2: Bits, value: Bits):
+        current_address = self._program_counter.value
+
+        or_result = self._or(self._registers.cf, self._registers.zf)
+
+        new_address = self._mux(current_address, value, or_result)
         self._program_counter.set(new_address)
--- tests/computer/instructions/test_jump.py
+++ tests/computer/instructions/test_jump.py
@@ -2,7 +2,7 @@
 Test for Jump instructions
 """
 from computer.data_types import Bits, Data16
-from computer.instructions.jump import JEQ, Jump
+from computer.instructions.jump import JEQ, JGE, JLT, Jump
 from computer.memory import SRAM
 from computer.program_counter import ProgramCounter
 from computer.registers import Registers
@@ -22,8 +22,27 @@ def test_jump(registers: Registers, sram: SRAM, program_counter: ProgramCounter)
 
     assert program_counter.value == d
 
+def test_jeq_zf_false(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
+    sram.reset()
+    registers.reset()
 
-def test_jeq_true(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
+    registers.zf = False
+
+    initial_pc_address = Bits(0, 0, 0, 0, 1, 1, 1, 1)
+    program_counter.set(initial_pc_address)
+    program_counter.clock_tick(True)
+
+    new_pc_address = Bits(0, 1, 0, 1, 0, 1, 0, 1)
+    operand = Bits([0] * 6 + new_pc_address)
+
+    jump = JEQ(registers, sram, program_counter)
+    operand = Bits([0] * 6 + new_pc_address)
+    jump(operand)
+    program_counter.clock_tick(True)
+
+    assert program_counter.value == initial_pc_address
+
+def test_jeq_zf_true(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
     sram.reset()
     registers.reset()
 
@@ -43,10 +62,51 @@ def test_jeq_true(registers: Registers, sram: SRAM, program_counter: ProgramCoun
 
     assert program_counter.value == new_pc_address
 
-def test_jeq_false(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
+def test_jlt_cf_false(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
     sram.reset()
     registers.reset()
 
+    registers.cf = False
+
+    initial_pc_address = Bits(0, 0, 0, 0, 1, 1, 1, 1)
+    program_counter.set(initial_pc_address)
+    program_counter.clock_tick(True)
+
+    new_pc_address = Bits(0, 1, 0, 1, 0, 1, 0, 1)
+    operand = Bits([0] * 6 + new_pc_address)
+
+    jump = JLT(registers, sram, program_counter)
+    operand = Bits([0] * 6 + new_pc_address)
+    jump(operand)
+    program_counter.clock_tick(True)
+
+    assert program_counter.value == new_pc_address
+
+def test_jlt_cf_true(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
+    sram.reset()
+    registers.reset()
+
+    registers.cf = True
+
+    initial_pc_address = Bits(0, 0, 0, 0, 1, 1, 1, 1)
+    program_counter.set(initial_pc_address)
+    program_counter.clock_tick(True)
+
+    new_pc_address = Bits(0, 1, 0, 1, 0, 1, 0, 1)
+    operand = Bits([0] * 6 + new_pc_address)
+
+    jump = JLT(registers, sram, program_counter)
+    operand = Bits([0] * 6 + new_pc_address)
+    jump(operand)
+    program_counter.clock_tick(True)
+
+    assert program_counter.value == initial_pc_address
+
+def test_jge_cf_false_zf_false(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
+    sram.reset()
+    registers.reset()
+
+    registers.cf = False
     registers.zf = False
 
     initial_pc_address = Bits(0, 0, 0, 0, 1, 1, 1, 1)
@@ -56,9 +116,72 @@ def test_jeq_false(registers: Registers, sram: SRAM, program_counter: ProgramCou
     new_pc_address = Bits(0, 1, 0, 1, 0, 1, 0, 1)
     operand = Bits([0] * 6 + new_pc_address)
 
-    jump = JEQ(registers, sram, program_counter)
+    jump = JGE(registers, sram, program_counter)
     operand = Bits([0] * 6 + new_pc_address)
     jump(operand)
     program_counter.clock_tick(True)
 
     assert program_counter.value == initial_pc_address
+
+def test_jge_cf_false_zf_true(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
+    sram.reset()
+    registers.reset()
+
+    registers.cf = False
+    registers.zf = True
+
+    initial_pc_address = Bits(0, 0, 0, 0, 1, 1, 1, 1)
+    program_counter.set(initial_pc_address)
+    program_counter.clock_tick(True)
+
+    new_pc_address = Bits(0, 1, 0, 1, 0, 1, 0, 1)
+    operand = Bits([0] * 6 + new_pc_address)
+
+    jump = JGE(registers, sram, program_counter)
+    operand = Bits([0] * 6 + new_pc_address)
+    jump(operand)
+    program_counter.clock_tick(True)
+
+    assert program_counter.value == new_pc_address
+
+def test_jge_cf_true_zf_false(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
+    sram.reset()
+    registers.reset()
+
+    registers.cf = True
+    registers.zf = False
+
+    initial_pc_address = Bits(0, 0, 0, 0, 1, 1, 1, 1)
+    program_counter.set(initial_pc_address)
+    program_counter.clock_tick(True)
+
+    new_pc_address = Bits(0, 1, 0, 1, 0, 1, 0, 1)
+    operand = Bits([0] * 6 + new_pc_address)
+
+    jump = JGE(registers, sram, program_counter)
+    operand = Bits([0] * 6 + new_pc_address)
+    jump(operand)
+    program_counter.clock_tick(True)
+
+    assert program_counter.value == new_pc_address
+
+def test_jge_cf_true_zf_true(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
+    sram.reset()
+    registers.reset()
+
+    registers.cf = True
+    registers.zf = True
+
+    initial_pc_address = Bits(0, 0, 0, 0, 1, 1, 1, 1)
+    program_counter.set(initial_pc_address)
+    program_counter.clock_tick(True)
+
+    new_pc_address = Bits(0, 1, 0, 1, 0, 1, 0, 1)
+    operand = Bits([0] * 6 + new_pc_address)
+
+    jump = JGE(registers, sram, program_counter)
+    operand = Bits([0] * 6 + new_pc_address)
+    jump(operand)
+    program_counter.clock_tick(True)
+
+    assert program_counter.value == new_pc_address
