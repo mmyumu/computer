5e49e26850fe629aa724c8df19dd2df5dd67896b
mmyumu <mmyumu@gmail.com>
2024-02-25 03:04:50 +0100

    Add JEQ instruction

--- computer/instructions/jump.py
+++ computer/instructions/jump.py
@@ -2,7 +2,11 @@
 Jump instructions module
 """
 from computer.data_types import Bits
+from computer.electronic.circuits.bitwise import BitwiseMux
 from computer.instructions.instruction import ControlInstruction
+from computer.memory import SRAM
+from computer.program_counter import ProgramCounter
+from computer.registers import Registers
 
 # pylint: disable=R0903
 class Jump(ControlInstruction):
@@ -12,3 +16,19 @@ class Jump(ControlInstruction):
     """
     def compute(self, reg1: Bits, reg2: Bits, value: Bits):
         self._program_counter.set(value)
+
+
+class JEQ(ControlInstruction):
+    """
+    Set the Program Counter to the immediate value if ZF = 1
+    JEQ IMD		; if ZF = 1, sets PC to the immediate 16-bit value
+    """
+    def __init__(self, registers: Registers, memory: SRAM, program_counter: ProgramCounter) -> None:
+        super().__init__(registers, memory, program_counter)
+        self._mux = BitwiseMux(2 ** self._registers.size)
+
+    def compute(self, reg1: Bits, reg2: Bits, value: Bits):
+        current_address = self._program_counter.value
+
+        new_address = self._mux(current_address, value, self._registers.zf)
+        self._program_counter.set(new_address)
--- tests/computer/instructions/test_jump.py
+++ tests/computer/instructions/test_jump.py
@@ -2,7 +2,7 @@
 Test for Jump instructions
 """
 from computer.data_types import Bits, Data16
-from computer.instructions.jump import Jump
+from computer.instructions.jump import JEQ, Jump
 from computer.memory import SRAM
 from computer.program_counter import ProgramCounter
 from computer.registers import Registers
@@ -21,3 +21,44 @@ def test_jump(registers: Registers, sram: SRAM, program_counter: ProgramCounter)
     program_counter.clock_tick(True)
 
     assert program_counter.value == d
+
+
+def test_jeq_true(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
+    sram.reset()
+    registers.reset()
+
+    registers.zf = True
+
+    initial_pc_address = Bits(0, 0, 0, 0, 1, 1, 1, 1)
+    program_counter.set(initial_pc_address)
+    program_counter.clock_tick(True)
+
+    new_pc_address = Bits(0, 1, 0, 1, 0, 1, 0, 1)
+    operand = Bits([0] * 6 + new_pc_address)
+
+    jump = JEQ(registers, sram, program_counter)
+    operand = Bits([0] * 6 + new_pc_address)
+    jump(operand)
+    program_counter.clock_tick(True)
+
+    assert program_counter.value == new_pc_address
+
+def test_jeq_false(registers: Registers, sram: SRAM, program_counter: ProgramCounter):
+    sram.reset()
+    registers.reset()
+
+    registers.zf = False
+
+    initial_pc_address = Bits(0, 0, 0, 0, 1, 1, 1, 1)
+    program_counter.set(initial_pc_address)
+    program_counter.clock_tick(True)
+
+    new_pc_address = Bits(0, 1, 0, 1, 0, 1, 0, 1)
+    operand = Bits([0] * 6 + new_pc_address)
+
+    jump = JEQ(registers, sram, program_counter)
+    operand = Bits([0] * 6 + new_pc_address)
+    jump(operand)
+    program_counter.clock_tick(True)
+
+    assert program_counter.value == initial_pc_address
