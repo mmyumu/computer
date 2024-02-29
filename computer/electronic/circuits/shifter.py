92fa26d643b5f87a1168878a37628f0036b3b38d
mmyumu <mmyumu@gmail.com>
2024-02-29 01:25:23 +0100

    Add shifter (rotation)

---
+++ computer/electronic/circuits/shifter.py
@@ -0,0 +1,58 @@
+"""
+Barrel shifter module
+"""
+from typing import Any, List
+
+from computer.data_types import Bits
+from computer.electronic.circuits.mux import MUX2To1
+
+
+class BarrelShifter:
+    """
+    Barrel shifter (aka bit rotation).
+    It does both right and left depending on the given parameter.
+    """
+    def __init__(self, size, right: bool = True) -> None:
+        self._size = size
+        self._right = right
+        self._muxes: List[List[MUX2To1]] = []
+
+        for _ in range(size):
+            muxes = []
+            for _ in range(2** size):
+                muxes.append(MUX2To1())
+            self._muxes.append(muxes)
+
+    def __call__(self, i: Bits, s: Bits) -> Any:
+        if len(s) != self._size:
+            raise ValueError(f"Length of s should be {self._size} but is {len(s)}")
+
+        if len(i) != 2 ** self._size:
+            raise ValueError(f"Length of i should be {2 ** self._size} but is {len(i)}")
+
+        if self._right:
+            current_i = i
+        else:
+            current_i = i[::-1]
+
+        for index_s, bit_s in enumerate(s[::-1]):
+            number_of_zero = 2 ** index_s
+
+            output = []
+            for index_i, bit_i in enumerate(current_i):
+                if index_i < number_of_zero:
+                    a1 = current_i[(2**self._size) - number_of_zero + index_i]
+                else:
+                    a1 = current_i[index_i - number_of_zero]
+
+                a0 = bit_i
+                mux_result = self._muxes[index_s][index_i](a0, a1, bit_s)
+
+                output.append(mux_result)
+            current_i = output
+
+        if self._right:
+            result = Bits(output)
+        else:
+            result = Bits(output[::-1])
+        return result
---
+++ tests/computer/electronic/circuits/test_barrel_shifter.py
@@ -0,0 +1,42 @@
+"""
+Test for barrel shifters
+"""
+import pytest
+from computer.data_types import Bits
+
+from computer.electronic.circuits.shifter import BarrelShifter
+
+
+# pylint: disable=C0116,W0212
+
+def rotr(num, bits):
+    num &= (2**bits-1)
+    bit = num & 1
+    num >>= 1
+    if bit:
+        num |= (1 << (bits-1))
+
+    return num
+
+@pytest.fixture(name="barrel_shifter_right")
+def fixture_barrel_shifter_right():
+    return BarrelShifter(3, right=True)
+
+@pytest.fixture(name="barrel_shifter_left")
+def fixture_barrel_shifter_left():
+    return BarrelShifter(3, right=False)
+
+
+def test_barrel_shifter(barrel_shifter_right: BarrelShifter, barrel_shifter_left: BarrelShifter):
+
+    for i in range(256):
+        i = Bits(i, size=8)
+
+        for s in range(8):
+            s = Bits(s, size=3)
+
+            result_right = barrel_shifter_right(i, s)
+            assert ((i.to_int() >> s.to_int())|(i.to_int() << (8 - s.to_int())) & 0xFF) % (2 ** 8) == result_right.to_int(), f"Inputs: i={i}, s={s}"
+
+            result_left = barrel_shifter_left(i, s)
+            assert ((i.to_int() << s.to_int())|(i.to_int() >> (8 - s.to_int())) & 0xFF) % (2 ** 8) == result_left.to_int(), f"Inputs: i={i}, s={s}"
